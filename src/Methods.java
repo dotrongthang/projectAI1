b0c32eddfc5e167255779a48a04c6eef6de5fa1a
Administrator <dotrongthangdb@gmail.com>
2020-09-21 15:01:55 +0700

    GUI

--- src/Methods.java
+++ src/Methods.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,23 +12,26 @@ public class Methods extends JFrame {
     public ArrayList<Clause> clauses = new ArrayList<>();
     public int q1 = 0, q2 = 0, q3 = 0, index;
     Rules rules = new Rules();
+    ArrayList<String> listResult = new ArrayList<>();
     JComboBox cbo1, cbo2, cbo3;  //combo box
     JTextField txtS1, txtS2, txtP1, txtP2, txtS3, txtP3; //lưu dữ liệu người dùng nhập
-    String[] arrayClause = {"Tất cả S là P", "Tất cả S không là P", "Gần như tất cả S là P",
-            "Gần như tất cả S không là P", "Hầu hết S là P", "Hầu hết S không là P"
-            , "Nhiều S là P", "Nhiều S không là P", "Một vài S là P", "Một vài S không là P"} ;
+    String[] arrayClause = {"All S are P", "No S are P", "Almost all S are P",
+            "Almost all S are not P", "Most S are P", "Most S are not P"
+            , "Many S are P", "Many S are not P", "Some S are P", "Some S are not P"} ;
 
     public void initUI(){
         addData(clauses);
         JTabbedPane myTable = new JTabbedPane();
         JPanel checkPn = new JPanel();
         JPanel showPn = new JPanel();
-        createClause1(checkPn, "Chọn loại tiền đề 1: ");
-        createClause2(checkPn, "Chọn loại tiền đề 2: ");
-        createClause3(checkPn, "Chọn loại mệnh đề: ");
+        createClause1(checkPn, "Choose type of clause 1: ");
+        createClause2(checkPn, "Choose type of clause 2: ");
+        createClause3(checkPn, "Choose type of clause 3: ");
         createButton(checkPn);
-        myTable.add(checkPn,"Kiểm tra");
-        myTable.add(showPn,"Xem kết quả");
+        myTable.add(checkPn,"Check");
+
+        createTable(showPn);
+        myTable.add(showPn,"Show Result");
 
         Container con = getContentPane();
         con.add(myTable);
@@ -38,28 +42,28 @@ public class Methods extends JFrame {
         jPanel.setLayout(null);
 
         JLabel lblclause = new JLabel(s);
-        lblclause.setBounds(20,50,120,25);
+        lblclause.setBounds(20,50,150,25);
         jPanel.add(lblclause);
 
         cbo1 = new JComboBox();
         addToCbo(cbo1, arrayClause);
-        cbo1.setBounds(140,50,190,25);
+        cbo1.setBounds(170,50,190,25);
         jPanel.add(cbo1);
 
-        JLabel lblS = new JLabel("Nhập S: ");
-        lblS.setBounds(340,50,50,25);
+        JLabel lblS = new JLabel("Input S: ");
+        lblS.setBounds(370,50,50,25);
         jPanel.add(lblS);
 
         txtS1 = new JTextField();
-        txtS1.setBounds(400,50,200,25);
+        txtS1.setBounds(430,50,200,25);
         jPanel.add(txtS1);
 
-        JLabel lblP = new JLabel("Nhập P: ");
-        lblP.setBounds(610,50,50,25);
+        JLabel lblP = new JLabel("Input P: ");
+        lblP.setBounds(640,50,50,25);
         jPanel.add(lblP);
 
         txtP1 = new JTextField();
-        txtP1.setBounds(670,50,200,25);
+        txtP1.setBounds(700,50,200,25);
         jPanel.add(txtP1);
     }
 
@@ -67,28 +71,28 @@ public class Methods extends JFrame {
         jPanel.setLayout(null);
 
         JLabel lblclause = new JLabel(s);
-        lblclause.setBounds(20,100,120,25);
+        lblclause.setBounds(20,100,150,25);
         jPanel.add(lblclause);
 
         cbo2 = new JComboBox();
         addToCbo(cbo2, arrayClause);
-        cbo2.setBounds(140,100,190,25);
+        cbo2.setBounds(170,100,190,25);
         jPanel.add(cbo2);
 
-        JLabel lblS = new JLabel("Nhập S: ");
-        lblS.setBounds(340,100,50,25);
+        JLabel lblS = new JLabel("Input S: ");
+        lblS.setBounds(370,100,50,25);
         jPanel.add(lblS);
 
         txtS2 = new JTextField();
-        txtS2.setBounds(400,100,200,25);
+        txtS2.setBounds(430,100,200,25);
         jPanel.add(txtS2);
 
-        JLabel lblP = new JLabel("Nhập P: ");
-        lblP.setBounds(610,100,50,25);
+        JLabel lblP = new JLabel("Input P: ");
+        lblP.setBounds(640,100,50,25);
         jPanel.add(lblP);
 
         txtP2 = new JTextField();
-        txtP2.setBounds(670,100,200,25);
+        txtP2.setBounds(700,100,200,25);
         jPanel.add(txtP2);
     }
 
@@ -96,28 +100,28 @@ public class Methods extends JFrame {
         jPanel.setLayout(null);
 
         JLabel lblclause = new JLabel(s);
-        lblclause.setBounds(20,150,120,25);
+        lblclause.setBounds(20,150,150,25);
         jPanel.add(lblclause);
 
         cbo3 = new JComboBox();
         addToCbo(cbo3, arrayClause);
-        cbo3.setBounds(140,150,190,25);
+        cbo3.setBounds(170,150,190,25);
         jPanel.add(cbo3);
 
-        JLabel lblS = new JLabel("Nhập S: ");
-        lblS.setBounds(340,150,50,25);
+        JLabel lblS = new JLabel("Input S: ");
+        lblS.setBounds(370,150,50,25);
         jPanel.add(lblS);
 
         txtS3 = new JTextField();
-        txtS3.setBounds(400,150,200,25);
+        txtS3.setBounds(430,150,200,25);
         jPanel.add(txtS3);
 
-        JLabel lblP = new JLabel("Nhập P: ");
-        lblP.setBounds(610,150,50,25);
+        JLabel lblP = new JLabel("Input P: ");
+        lblP.setBounds(640,150,50,25);
         jPanel.add(lblP);
 
         txtP3 = new JTextField();
-        txtP3.setBounds(670,150,200,25);
+        txtP3.setBounds(700,150,200,25);
         jPanel.add(txtP3);
     }
 
@@ -129,18 +133,18 @@ public class Methods extends JFrame {
     }
 
     public void createButton(JPanel jPanel){
-        JButton okBtn = new JButton("Kiểm tra");
+        JButton okBtn = new JButton("Check");
         okBtn.setBounds(300,250,100,40);
         okBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showConfirmDialog(null,checkResult(),"Kết quả",0);
+                JOptionPane.showMessageDialog(null,checkResult(),"Result",1);
 
             }
         });
         jPanel.add(okBtn);
 
-        JButton exitBtn = new JButton("Thoát");
+        JButton exitBtn = new JButton("Exit");
         exitBtn.setBounds(500,250,100,40);
         exitBtn.addActionListener(new ActionListener() {
             @Override
@@ -179,7 +183,7 @@ public class Methods extends JFrame {
         index = checkFigure();
 
         if (index == -1){
-            s = "Vui lòng nhập đầy đủ thông tin!";
+            s = "Please enter enough information!";
         }else {
             s = rules.rule1(clauses.get(q1),
                 clauses.get(q2), clauses.get(q3), index) +"-"+
@@ -220,7 +224,26 @@ public class Methods extends JFrame {
 //    }
 
 
+    public void createTable(JPanel jPanel){
+        jPanel.setLayout(new FlowLayout());
+        JButton showBtn = new JButton("Show");
+        showBtn.setBounds(440,20,70,25);
+        showBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showBtn.setVisible(false);
+                showResult(jPanel);
+                JLabel jLabel = new JLabel(listResult.toString());
+                jPanel.add(jLabel);
+            }
+        });
+        jPanel.add(showBtn);
 
+//        JLabel lblResult = new JLabel(showResult(jPanel));
+//        lblResult.setBounds(20,100,910,200);
+//        jPanel.add(lblResult);
+
+    }
 
     public boolean checkResult(Clause q1, Clause q2, Clause q3, int figure){
         if (rules.rule1(q1,q2,q3,figure) && rules.rule2(q1,q2,q3,figure) &&
@@ -232,27 +255,24 @@ public class Methods extends JFrame {
 
     }
 
-    public void showResult(){
-        int index = 0;
+    public String showResult(JPanel jPanel){
+        String s = "";
+        int index12 = 0;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 for (int k = 0; k < 10; k++) {
                     for (int l = 1; l <= 4; l++) {
                         if (checkResult(clauses.get(i), clauses.get(j), clauses.get(k), l)){
-                            if (index % 5 == 0){
-                                System.out.println();
-                            }
-                            System.out.print(clauses.get(i).getSign() + clauses.get(j).getSign() +
-                                    clauses.get(k).getSign() + "-" + l +  ":" + "TTTT" + "\t");
-                            index++;
+                            String string = (clauses.get(i).getSign() + clauses.get(j).getSign() +
+                                    clauses.get(k).getSign() + "-" + l +  ":" + "TTTT");
+                            listResult.add(string);
+                            index12++;
                         }
                     }
                 }
             }
         }
-        System.out.println();
-        System.out.println("---------------------------------");
-        System.out.println("Có " + index + " kết quả phù hợp");
+        return s;
     }
 
 }
